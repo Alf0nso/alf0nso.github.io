d3b4656c914428999841e166e5fe82ad80255b02
Afonso Rafael <afonsorafael@sapo.pt>
2025-08-15 00:33:30 +0200

    Clean,Add[references]: finally managed to add references to the site
    Cleaned a bit, but there is still a some code that might be not so
    necessary. It would also make quite a lot of sense to document better
    at some point...

--- site.hs
+++ site.hs
@@ -1,60 +1,42 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE BlockArguments    #-}
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Monad (liftM)
-import           Data.Monoid (mappend)
+import           Control.Monad                (liftM)
+import           Data.Monoid                  (mappend)
 import           Data.Maybe
+import qualified Data.Map.Strict        as Map
 import           Hakyll
 import           Hakyll.Core.Compiler
+import           Text.Pandoc.Highlighting
 import           Text.Pandoc.Options
+import           Text.Pandoc.Definition
+import           Text.Pandoc.Walk             (walk, walkM)
 import           System.FilePath
-import           System.Directory (createDirectoryIfMissing)
-import           System.FilePath.Posix (takeDirectory)
+import           System.Directory             (createDirectoryIfMissing)
+import           System.FilePath.Posix        (takeDirectory)
 
--- Helper functions
+-- | Helper functions --------------------------------------
 createAndWriteFile :: FilePath -> String -> IO ()
 createAndWriteFile path content = do
   createDirectoryIfMissing True $ takeDirectory path
   writeFile path content
 
--- | My own configuration for the website
-config :: Configuration
-config = defaultConfiguration
-         { destinationDirectory = "docs"
-         , previewPort          = 5000 }
--- --------------------------------------
+-- Parametric biblio processor
+processBib :: Item CSL -> Item Biblio -> Item Pandoc -> Compiler (Item Pandoc)
+processBib csl bib pandoc = do
+  pandoc' <- withItemBody
+      (\(Pandoc (Meta meta) bs) -> pure $
+        Pandoc (Meta $ Map.insert "link-citations" (MetaBool True) meta)
+        bs)
+      pandoc
+  fmap insertRefHeading <$> processPandocBiblio csl bib pandoc'
+  where
+    insertRefHeading :: Pandoc -> Pandoc
+    insertRefHeading = walk $ concatMap \case
+      d@(Div ("refs", _, _) _) ->
+        [Header 2 ("references", [], []) [Str "References"], d]
+      block -> [block]
 
-postCtx :: Context String
-postCtx = dateField "published" "%B %e, %Y"
-          <> defaultContext
-
-posts :: Context String
-posts = listField "posts" postCtx $
-        do posts <- recentFirst =<< loadAll "main/posts/en/*"
-           return posts
-
-archiveCtx :: Context String
-archiveCtx = posts
-             <> defaultContext             
--- --------------------------------------
-
-{- Include MathJax in the pandoc options so it is possible
-to render pretty math equations. -}
-pandocWOptions :: WriterOptions
-pandocWOptions = defaultHakyllWriterOptions
-                { writerHTMLMathMethod = MathJax "" }
-
-{- My personal website compiler with my preferences. -}
-grassBiblioCompiler :: Compiler (Item String)
-grassBiblioCompiler = do
-  csl <- load "bib/tpls.csl"
-  bib <- load "bib/global.bib"
-  getResourceBody
-    >>= readPandocBiblio defaultHakyllReaderOptions csl bib
-    >>= pure . (writePandocWith pandocWOptions)
-
-grassCompiler :: Compiler (Item String)
-grassCompiler = pandocBiblioCompiler "bib/acm.csl" "bib/global.bib"
-
-{- Used to process the main contents of the site. -}
 baseFolderAndHtml :: Routes
 baseFolderAndHtml =
   customRoute $ takeFileName . (`replaceExtension` "html") . toFilePath
@@ -64,6 +46,12 @@ folderAndHtml path =
     customRoute $
     (++) path . takeFileName . (`replaceExtension` "html") . toFilePath
 
+-- Functions to process file paths
+orgToHtml :: FilePath -> FilePath
+orgToHtml fp = base ++ (file `replaceExtension` "html")
+  where
+    (base, file) = splitFileName fp
+
 switchMainFolderFor :: FilePath -> Routes
 switchMainFolderFor path =
   customRoute $ (++) path . takeFileName . toFilePath
@@ -71,17 +59,60 @@ switchMainFolderFor path =
 removeMainFolder :: Routes
 removeMainFolder = gsubRoute "main/" (const "")
 
--- If there is a draft field and it has a value other than false, then
--- the site generator will not publish it/consider it
+noMainAndHtml :: Routes
+noMainAndHtml = composeRoutes (customRoute $ orgToHtml . toFilePath) removeMainFolder
+
+-- Function to filter drafts, to avoid compiling and uploading them
 filterDrafts :: Metadata -> Bool
 filterDrafts meta = maybe True (=="false") $ lookupString "draft" meta
-------------------------------------------------------------------------
+
+-- The compilers I use to generate the website
+grassBiblioCompiler :: Compiler (Item String)
+grassBiblioCompiler = do
+  csl <- load "bib/acm.csl"    :: Compiler (Item CSL)
+  bib <- load "bib/global.bib" :: Compiler (Item Biblio)  
+  getResourceBody
+    >>= readPandocWith defaultHakyllReaderOptions
+    >>= processBib csl bib
+    >>= pure . (writePandocWith pandocWOptions)
+
+grassCompiler :: Compiler (Item String)
+grassCompiler = pandocBiblioCompiler "bib/acm.csl" "bib/global.bib"
+------------------------------------------------------------
+
+
+-- | Configurations and data to use after ------------------
+config :: Configuration
+config = defaultConfiguration
+         { destinationDirectory = "docs"
+         , previewPort          = 5000 }
+
+postCtx :: Context String
+postCtx = dateField "published" "%B %e, %Y"
+          <> defaultContext
+
+posts :: Context String
+posts = listField "posts" postCtx $
+        do posts <- recentFirst =<< loadAll "main/posts/*/*"
+           return posts
+
+archiveCtx :: Context String
+archiveCtx = posts
+             <> defaultContext
+
+-- Include MathJax in the pandoc options so it is possible
+-- to render pretty math equations.
+pandocWOptions :: WriterOptions
+pandocWOptions = defaultHakyllWriterOptions
+                { writerHTMLMathMethod = MathJax "" }
+------------------------------------------------------------
+
 
 buildSite :: IO ()
 buildSite = hakyllWith config $ do
   match "bib/global.bib" $ compile biblioCompiler
-  match "bib/acm.csl" $ compile cslCompiler  
-  match "templates/*" $ compile templateBodyCompiler
+  match "bib/acm.csl"    $ compile cslCompiler  
+  match "templates/*"    $ compile templateBodyCompiler
 
   match "css/*" $ do
     route   idRoute
@@ -118,20 +149,27 @@ buildSite = hakyllWith config $ do
         >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
         >>= loadAndApplyTemplate "templates/default.html" archiveCtx
         >>= relativizeUrls
-  
-  matchMetadata "main/posts/en/*" filterDrafts $ do
-    route     baseFolderAndHtml
-    compile $ grassCompiler
+
+  -- All posts will devided by language folders for now.
+  -- 
+  matchMetadata "main/posts/*/**.org" filterDrafts $ do
+    route   $ noMainAndHtml
+    compile $ grassBiblioCompiler
       >>= loadAndApplyTemplate "templates/post.html"    postCtx
       >>= loadAndApplyTemplate "templates/default.html" postCtx
       >>= relativizeUrls
 
-  matchMetadata "main/posts/pt/*" filterDrafts $ do
-   route     baseFolderAndHtml
-   compile $ grassCompiler
-     >>= loadAndApplyTemplate "templates/post.html"    postCtx
-     >>= loadAndApplyTemplate "templates/default.html" postCtx
-     >>= relativizeUrls
+  -- Presentations
+  match "main/presentations/*/style.css" $ do
+    route   removeMainFolder
+    compile compressCssCompiler
+
+  match ("main/presentations/*/**.html"
+         .||. "main/presentations/*/**.png"
+         .||. "main/presentations/*/**.jpeg"
+        ) $ do
+    route   $ removeMainFolder
+    compile copyFileCompiler
 
 main :: IO ()
 main = buildSite
